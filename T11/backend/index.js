9186afb9481d265fdb97d6ed95321700af523451
mughals9 <saood.mughal@mail.utoronto.ca>
2025-11-23 16:18:58 -0500

    .;

--- T11/backend/index.js
+++ T11/backend/index.js
@@ -1,14 +1,19 @@
-require("dotenv").config
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import cors from "cors"; 
 import routes from "./routes.js";
 
 const app = express();
 
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
 app.use(cors({
     origin: FRONTEND_URL,
-    credentials: true
+    credentials: true,
 }));
+
 app.use(express.json());
+app.use(routes);
 
 export default app;
